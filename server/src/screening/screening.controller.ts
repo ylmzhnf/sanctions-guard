d93a05eac9bab060237bf525d7a88ecdad42449e
ylmzhnf <ylmzhnf@gmail.com>
2026-02-03 19:23:13 +0300

    feat(screening): add GET/screening/search enpoint (#4)

--- server/src/screening/screening.controller.ts
+++ server/src/screening/screening.controller.ts
@@ -1,7 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { SearchSanctionDto } from './dto/search-sanction.dto';
+import { ScreeningService } from './screening.service';
 
 @Controller('screening')
 export class ScreeningController {
-  @Get()
-  searchSanctionedNames() {}
+  constructor(private readonly screeningService: ScreeningService) {}
+  @Get('search')
+  async search(@Query() query: SearchSanctionDto) {
+    const result = await this.screeningService.searchSanctionedNames(
+      query.queryName,
+      query.userId,
+    );
+    console.log('Screening search result:', result);
+
+    const formattedResult = result.map((item) => ({
+      ...item,
+      score: Number(item.score.toFixed(2)),
+    }));
+
+    return {
+      success: true,
+      count: formattedResult.length,
+      data: formattedResult,
+    };
+  }
 }
