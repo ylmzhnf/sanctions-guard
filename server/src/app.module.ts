1aa91343cc47c1dad2ba0cf28009ed291acf7c81
ylmzhnf <ylmzhnf@gmail.com>
2026-02-04 14:33:59 +0300

    chore: install auth dependencies (passport, jwt, bcrypt) (#5)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ScreeningModule } from './screening/screening.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     PrismaModule,
     ScreeningModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
